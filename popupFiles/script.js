a14a77616059023b1f8a131a9a38a8872c90eb77
Ahmedexe <alzuhairahmed@gmail.com>
2024-01-17 17:56:42 +0300

    doomsday algorithm implementation

--- popupFiles/script.js
+++ popupFiles/script.js
@@ -27,5 +27,56 @@ function isNumber(value) {
 }
 
 function DoomsDayAlg(year, month, day) {
-    
+
+    // define some functions/lists and needed info
+
+    let days = ["Sunday", "Monday", "Tuesday", "Wednseday", "Thusrday", "Friday", "Saturday"]
+
+    let isLeap = () => {
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
+
+
+    let anchorDayIdx = () => {
+        let c = Math.floor(year / 100);
+        return c % 4
+    }
+
+
+
+    // calculation 1:
+    let calc1 = Math.floor((year % 100) / 12)
+
+
+    // calculation 2:
+    let calc2 = (year % 100) - (calc1 * 12);
+
+
+    // calculation 3:
+    let calc3 = Math.floor(calc2 / 4);
+
+    // calculation 4:
+    let calc4 = anchorDayIdx();
+
+    // Doomsday of the year:
+    let sumOfCalcs = calc1 + calc2 + calc3 + calc4;
+    let doomsDayIdx = sumOfCalcs % 7
+
+
+
+    //test
+    return days[doomsDayIdx];
+
 }
